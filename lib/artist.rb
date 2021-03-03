2777284d78ad511eaaec49084c2cd4c7100b1511
Jason Whitlock <jrwhitlock7@gmail.com>
2021-03-02 18:02:38 -0700

    commit tree f351b1e38ac9a7ad632718b22f39a216f8a46312

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,32 @@
+require 'pry'
+class Artist
+    attr_accessor :name, :genre
+    @@all =[]
 
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+        # binding.pry
+    end
+
+    def songs #returns all songs associated with that artist
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def genres #has mant genres through songs
+        songs.map{|song| song.genre}
+    end
+
+end
--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,30 @@
+class Doctor
+    attr_reader :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments #returns all appointments associated with that docotor
+        Appointment.all.select {|appointment| appointment.doctor == self}
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def patients
+        appointments.map {|appointment| appointment.patient}
+    end
+end
